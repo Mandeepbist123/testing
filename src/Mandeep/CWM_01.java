2d7e3ec28940a3aba2c534300c4f6aa76c48000b
Mandeepbist123 <mandeepbist123@gmail.com>
2025-05-08 11:57:32 +0530

    java bug fix

--- src/Mandeep/CWM_01.java
+++ src/Mandeep/CWM_01.java
@@ -2,6 +2,6 @@ package Mandeep;
 
 public class CWM_01 {
     public static void main(String[] args) {
-        System.out.println("Hello Mandeep");
+        System.out.println("Hello Mandeep Bist and iam learning java");
     }
 }
