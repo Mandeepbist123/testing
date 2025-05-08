00853fd598a89c007ebc70ca426574f9439157e6
Mandeepbist123 <mandeepbist123@gmail.com>
2025-05-08 11:28:54 +0530

    testing Hello

---
+++ src/javalearner/hello.java
@@ -0,0 +1,23 @@
+package javalearner;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("Jaydev");
+
+        int a=56;
+        System.out.println(a);
+        a=20;
+        System.out.println(a);
+
+        String name="jaydev";
+        System.out.println(name);
+
+        int i=50;
+        double d=i;
+        System.out.println(i);
+        System.out.println(d);
+
+
+    }
+}
